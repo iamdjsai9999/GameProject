645c2fc7d35f99d4fc507d42043a3e6bb4b81163
Dj Sai <87787448+iamdjsai9999@users.noreply.github.com>
2023-02-08 17:13:45 +0530

    Create sneload.py

---
+++ Smooth Af/sneload.py
@@ -0,0 +1,9 @@
+# Initialising pygame
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('GeeksforGeeks Snakes')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
